2af84dabaf877b0a0976df6cd8492302c72a502c
pranjal0416 <singhp4@go.stockton.edu>
2024-09-22 14:46:14 -0400

    stats

--- src/SetOperations.java
+++ src/SetOperations.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class SetOperations {
+
+    public static ArrayList<Integer> union(ArrayList<Integer> list1, ArrayList<Integer> list2)
+    {
+        ArrayList<Integer> unionList = new ArrayList<>(list1);
+        for (Integer element : list2) {
+            if (!unionList.contains(element)) {
+                unionList.add(element);
+            }
+        }
+    return unionList;
+    }
+    public static ArrayList<Integer> intersect(ArrayList<Integer> list1, ArrayList<Integer> list2)
+    {
+        ArrayList<Integer> intersectList = new ArrayList<Integer>();
+        for (Integer element : list1) {
+            if (list2.contains(element) && !intersectList.contains(element)) {
+                intersectList.add(element);
+            }
+        }
+        return intersectList;
+    }
+    public static ArrayList<Integer> complement(ArrayList<Integer> allValues, ArrayList<Integer> subset)
+    {
+        ArrayList<Integer> compList = new ArrayList<Integer>();
+        for (Integer element : allValues) {
+            if (!subset.contains(element)) {
+                compList.add(element);
+            }
+        }
+        return compList;
+    }
+
 }
--- src/StatsLib.java
+++ src/StatsLib.java
@@ -1,4 +1,65 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StatsLib {
+
+
+    public double computeMean(ArrayList<Integer> listOfNumbers){
+        int sum = 0;
+        for (int singleNumber:listOfNumbers)
+        {
+            sum = sum + singleNumber;
+        }
+        return sum / (double) listOfNumbers.size();
+    }
+    public double computeMedian(ArrayList<Integer> listOfNumbers)
+    {
+        double median = 0;
+        listOfNumbers.sort(Integer::compareTo);
+        int  midPoint = listOfNumbers.size()/2;
+        if (listOfNumbers.size()%2 == 0){
+            median = (listOfNumbers.get(midPoint)+listOfNumbers.get(midPoint-1))/2;
+        }
+        else{
+            median = listOfNumbers.get(midPoint);
+        }
+        return median;
+    }
+
+    public ArrayList<Integer> computeModes(ArrayList<Integer> numbers) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (Integer number : numbers) {
+            frequencyMap.put(number, frequencyMap.getOrDefault(number, 0) + 1);
+        }
+
+        int maxFrequency = 0;
+        for (Integer frequency : frequencyMap.values()) {
+            if (frequency > maxFrequency) {
+                maxFrequency = frequency;
+            }
+        }
+
+        ArrayList<Integer> modes = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            if (entry.getValue() == maxFrequency) {
+                modes.add(entry.getKey());
+            }
+        }
+
+        return modes;
+    }
+
+    public double standardDeviation(ArrayList<Integer> listOfNumbers)
+    {
+        double mean = computeMean(listOfNumbers);
+        double varianceSum = 0.0;
+        for (double number : listOfNumbers) {
+            varianceSum += Math.pow(number - mean, 2);
+        }
+        double variance = varianceSum / listOfNumbers.size();
+        return Math.sqrt(variance);
+    }
+
+
 }
